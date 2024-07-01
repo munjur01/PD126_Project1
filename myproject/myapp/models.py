0ccb21f5b755558bba54fd44e860f570aade973f
Munjur Alom <munjuralom.cse@gmail.com>
2024-07-01 11:29:19 +0600

    All page are static, I create UserInfo Model & base/layouts page

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class UserInfo(models.Model):
+    name = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='user_info_images/')
+
+    def __str__(self):
+        return self.name
